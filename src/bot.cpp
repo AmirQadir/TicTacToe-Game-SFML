ce00538dcdc293afd7a9ddef0c730e01056fa223
AmirAli <k163641@nu.edu.pk>
2017-05-06 18:36:05 +0500

    changes

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,8 +1,11 @@
 #include "bot.h"
-
+#include <iostream>
 Bot::Bot()
 {
-    //ctor
+
+    //for NOW
+    type = "EASY";
+
 }
 
 Bot::Bot(int a,int b)
@@ -15,4 +18,44 @@ Bot::Bot(int a,int b)
 Bot::~Bot()
 {
 
+}
+void Bot::Delay(double s)
+{
+    int sec = int(s*1000);
+    Sleep(sec);
+}
+
+
+void Bot::placePiece(int board[3][3])
+{
+    Delay(1);
+    //it will read the board
+    //then set any one 0 to 2
+    bool placed = false;
+
+    if(type=="EASY")
+    {
+
+        while(placed==false) //will keep generating random array positions until if finds one that is empty.
+        {
+            int a = rand()%3;
+            int b = rand()%3;
+
+            std::cout << a << " " << b << std::endl;
+
+            if(board[a][b]==0)
+            {
+                std::cout << "Placing" << std::endl;
+
+                board[a][b]=2;
+                placed = true;
+            }
+
+        }
+
+    }
+    else //for hard difficulty
+    {
+
+    }
 }
--- src/game.cpp
+++ src/game.cpp
@@ -64,82 +64,158 @@ void Game::HandleInput()
 
 void Game::Update()
 {
-    m_window.Update(board);//pass reference to board array
-    // this is for 1 cross
-    // sp_cross_1 k liyay positions k liyay karo then again u have to make a texture sp_cross_2
+     bool botTurn = false; //below function will return 1 (true) if user has placed a piece
+                            //only then we will call myBot.placePiece()
 
-    int i=0;
+     botTurn = m_window.Update(board);//pass reference to board array to take input from user
+                            //and set the corresponding cell to 1
+                            //it will return TRUE for botTurn if user places a piece (clicks mouse)
 
-    if(board[0][0]==1)
+    if(botTurn==true)
     {
-        std::cout << "INSIDE if COND\n";
-        sp_cross[i].setPosition(0,0);
+        myBot.placePiece(board);
     }
-    if(board[0][1]==1)
-    {
-
-        sp_cross[i].setPosition(200,0);
-        i++;
-    }
-    if(board[0][2]==1)
-    {
-        sp_cross[i].setPosition(400,0);
-    i++;
-    }
-    if(board[1][0]==1)
-    {
-        sp_cross[i].setPosition(0,200);
-    i++;
-    }
-    if(board[1][1]==1)
-    {
-        sp_cross[i].setPosition(0,400);
-    i++;
-    }
-    if(board[1][2]==1)
-    {
-        sp_cross[i].setPosition(0,600);
-    i++;
-    }
-
-    //for bot
-    int y=0;
-     if(board[0][0]==2)
-    {
-        std::cout << "Entrance of Bot\n";
-        bsprite[y].setPosition(0,0);
-    }
-    if(board[0][1]==2)
-    {
-
-        bsprite[y].setPosition(200,0);
-        y++;
-    }
-    if(board[0][2]==2)
-    {
-        bsprite[y].setPosition(400,0);
-    y++;
-    }
-    if(board[1][0]==2)
-    {
-        bsprite[y].setPosition(0,200);
-    y++;
-    }
-    if(board[1][1]==2)
-    {
-        bsprite[y].setPosition(0,400);
-    y++;
-    }
-    if(board[1][2]==2)
-    {
-        bsprite[y].setPosition(0,600);
-    y++;
-    }
+                            //now pass the board arrray to bot object
+                            //to read the already entered USER's piece and
+                            //place bot piece accordingly
 
 
-    // end of bot
+
+    /* BELOW IS DISPLAY ONLY */
+
+    //these are only to read board array and set position of objects
+    int i=0; //for CROSS ONLY
+    int y=0; // for O, BOT
+
+    /*
+
+    Every time UPDATE is called from the main()
+    it will first remove all pieces from the board (setposition away)
+    then will place one at a time (will increment i after placing one piece)
+    so that if any other IF condition is satisfied it will place next
+    PIECE from the array
+
+    */
+    for(int i=0;i<5;i++)
+    {
+        sp_cross[i].setPosition(-200,0);
+        bsprite[i].setPosition(-200,0);
+    }
+
+    /*
+
+    END OF BLOCK
+
+    */
+
+    //displaying the board. Making scope for clarity
+    {
 
 
+        if(board[0][0]==1)
+        {
+       //     std::cout << "INSIDE if COND\n";
+            sp_cross[i].setPosition(0,0);
+            i++;
+        }
+        if(board[0][1]==1)
+        {
+            sp_cross[i].setPosition(200,0);
+            i++;
+        }
+        if(board[0][2]==1)
+        {
+            sp_cross[i].setPosition(400,0);
+            i++;
+        }
+        if(board[1][0]==1)
+        {
+            sp_cross[i].setPosition(0,200);
+            i++;
+        }
+        if(board[1][1]==1)
+        {
+            sp_cross[i].setPosition(200,200);
+            i++;
+        }
+        if(board[1][2]==1)
+        {
+            sp_cross[i].setPosition(400,200);
+            i++;
+        }
+        if(board[2][0]==1)
+        {
+            sp_cross[i].setPosition(0,400);
+            i++;
+        }
+        if(board[2][1]==1)
+        {
+            sp_cross[i].setPosition(200,400);
+            i++;
+        }
+        if(board[2][2]==1)
+        {
+            sp_cross[i].setPosition(400,400);
+            i++;
+        }
+
+        /*
+
+        BOT FROM HERE
+
+        */
+
+         if(board[0][0]==2)
+        {
+    //        std::cout << "Entrance of Bot\n";
+            bsprite[y].setPosition(0,0);
+            y++;
+        }
+        if(board[0][1]==2)
+        {
+
+            bsprite[y].setPosition(200,0);
+            y++;
+        }
+        if(board[0][2]==2)
+        {
+            bsprite[y].setPosition(400,0);
+            y++;
+        }
+        if(board[1][0]==2)
+        {
+            bsprite[y].setPosition(0,200);
+            y++;
+        }
+        if(board[1][1]==2)
+        {
+            bsprite[y].setPosition(200,200);
+            y++;
+        }
+        if(board[1][2]==2)
+        {
+            bsprite[y].setPosition(400,200);
+            y++;
+        }
+        if(board[2][0]==2)
+        {
+            bsprite[y].setPosition(0,400);
+            y++;
+        }
+        if(board[2][1]==2)
+        {
+            bsprite[y].setPosition(200,400);
+            y++;
+        }
+        if(board[2][2]==2)
+        {
+            bsprite[y].setPosition(400,400);
+            y++;
+        }
+
+        // end of bot
+
+    }
 
 }
 
